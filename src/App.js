be765c683acd3c2ead4c4829a652e5669bec43f7
Jakodera <you@example.com>
2020-10-21 13:08:22 +0300

    Added Redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import './App.css';
+import {Provider} from 'react-redux'
 
 import Posts from './Components/Posts';
 import PostsForm from './Components/PostsForm';
+import { applyMiddleware, createStore } from 'redux';
+
+
+const store = createStore(() =>[], {}, applyMiddleware())
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <header>
         <PostsForm />
         <Posts />
       </header>
     </div>
+    </Provider>
   );
 }
 
